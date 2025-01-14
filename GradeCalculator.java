a270e4b3ba623ac6d7ce494721953f67aa6375b5
awaley0 <awaleprince17@gmail.com>
2025-01-14 08:07:03 +0545

    kei thapeko chuu

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -1,6 +1,5 @@
 public class GradeCalculator {
     public static void main(String[] args) {
-        Student student = new Student("Awale", 20, "Patan");
-        student.display();
+        System.out.println("Grade Calculator");
     }
 }
---
+++ main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        Student student = new Student("Awale", 20, "Patan");
+        student.display();
+    }
+}
